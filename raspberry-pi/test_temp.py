5b904fda06647d4c5e62f2d5e778be14998438f2
lblanc2003 <117928555+lblanc2003@users.noreply.github.com>
2024-12-23 19:28:20 +0100

    Create test_temp.py

---
+++ raspberry-pi/test_temp.py
@@ -0,0 +1,7 @@
+import adafruit_dht
+import board
+
+dhtDevice = adafruit_dht.DHT22(board.D4)
+temperature = dhtDevice.temperature
+humidity = dhtDevice.humidity
+print('Temp: {:.1f} C  Humidity: {}%'.format(temperature, humidity))
